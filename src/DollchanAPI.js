0177e6f9f9bd3c21c9257e0a0515efba563428c4
lahtabot <apeenya@yandex.com>
2020-06-16 21:19:31 +0900

    Add drawPatternOnImage()

--- src/DollchanAPI.js
+++ src/DollchanAPI.js
@@ -34,16 +34,30 @@
     const FONT_SIZE = 38;
     const FONT = `${38}px 'Oswald', 'Open Sans'`;
     const MAX_TEXT_WIDHT = 500;
+    const TEXT_ALIGN = 'center'; // works ok only with 'center' and 'left'
+
+    const X_OFFSET = 2 * FONT_SIZE;
+    const Y_OFFSET = 2 * FONT_SIZE;
 
     // Color, the last parameter is opacity.
-    const FILL_STYLE = "rgba(250, 250, 250,0.11)";
+    const FILL_STYLE = "rgba(255, 255, 255, 0.65)";
 
     // Shadow.
-    const SHADOW_COLOR = 'grey';
+    const SHADOW_COLOR = 'black';
     const SHADOW_BLUR = 3; // pixels
 
     // Rotate angle in radians.
-    const ROTATION_ANGLE = 45 * Math.PI / 180; // degrees * Math.PI / 180
+    const ROTATION_ANGLE = 40 * Math.PI / 180; // degrees * Math.PI / 180
+
+    // Unfortunately, CanvasPattern doesn't stretch (maybe coz of calling rotate()).
+    // You gotta try and see what values look ok.
+    const PATTERN_WIDTH = 400;
+    const PATTERN_HEIGHT = 400;
+
+
+    // Describes image type that will be returned by canvas.
+    const IMAGE_MIME_TYPE = 'image/jpeg'; // default is 'image/png'
+
 
     /************************************************************************
      * Dollchan API
@@ -117,40 +131,82 @@
 
 
     /************************************************************************
-     * File processing
+     * File processing.
     *************************************************************************/
 
     /**
-     * Reads images. Calls onImageLoad() when image is loaded;
-     * @param {File} file 
+     * Reads image from file.
+     * @param {File} file
+     * @returns {Promise<HTMLImageElement>} image
      */
-    function processFile(file) {
+    async function fileToImage(file) {
         if (!file) {
             log('readFile(file): no file!');
             return;
         }
 
-        if (!file.type.startsWith('image/')) { // TODO: process webm/mp4?
+        // TODO: process webm/mp4?
+        if (!file.type.startsWith('image/')) {
             log('readFile(file): not an image, ingoring! ' + file.type);
         }
-        
+
         const fileReader = new FileReader();
 
-        fileReader.onload = function (readerEvent) {
-            const image = new Image();
+        // Resolves with the image.
+        return new Promise((resolve, rej) => {
 
-            image.onload = onImageLoad;
-            image.src = readerEvent.target.result;
-        };
+            // When file is load.
+            fileReader.onload = function (readerEvent) {
+                const image = new Image();
 
-        fileReader.readAsDataURL(file);
+                image.onload = function (e) {
+                    resolve(image);
+                };
+
+                image.src = readerEvent.target.result;
+            };
+
+            fileReader.readAsDataURL(file);
+
+        });
+    }
+
+
+    /*************************************************************************
+     * Drawing.
+    **************************************************************************/
+
+    /**
+     * Applies pattern to image and return the resulting canvas.
+     * @param {HTMLImageElement} image 
+     * @param {CanvasPattern} pattern 
+     */
+    function drawPatternOnImage(image, pattern) {
+        const canvas = document.createElement('canvas');
+        // Set proper size.
+        canvas.width = image.width;
+        canvas.height = image.height;
+        const ctx = canvas.getContext("2d");
+        // Draw image on the canvas.
+        ctx.drawImage(image, 0, 0);
+
+        // Use generated pattern to make a watermark.
+        ctx.fillStyle = pattern;
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        return canvas;
+    }
+
+    /*************************************************************************
+    **************************************************************************/
+
     }
 
     /*************************************************************************
      * Helpers
     **************************************************************************/
 
-    
+
     function log() {
         console.log('2CH-WATERMARK: ', ...arguments);
     }
